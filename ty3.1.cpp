359af679c96ff0c589f9535b201b2450a818d9ea
kfalconspb <kfalconspb@bollingerusa.com>
2018-09-04 01:41:38 -0700

    3.1

--- ty3.1.cpp
+++ ty3.1.cpp
@@ -5,5 +5,20 @@ int main()
 {
 	cout << "This program will help you multiply two numbers" << endl;
 
-	cout << "Enter
+	cout << "Enter the first number: ";
+	int firstNumber = 0;
+	cin >> firstNumber;
+
+	cout << "Enter the second number: ";
+	int secondNumber = 0;
+	cin >> secondNumber;
+
+	// Multiply two numbers, store result in a variable
+	int multiplicationResult = firstNumber * secondNumber;
+
+	// Display result
+	cout << firstNumber << " x " << secondNumber;
+	cout << " = " << multiplicationResult << endl;
+
+	return 0;
 }
