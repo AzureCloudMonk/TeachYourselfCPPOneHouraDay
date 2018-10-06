912c62086fa0f33447c99fd2daeb835b6e935f8c
Scott Bollinger <kfalconspb@bollingerusa.com>
2018-10-05 21:26:48 -0700

    8.5

---
+++ ty8.4.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int age = 30;
+	int dogsAge = 9;
+	int testme = 187;
+	cout << "Integer age = " << age << endl;
+	cout << "Integer dogsAge = " << dogsAge << endl;
+
+	int* pointsToInt = &age;
+	cout << "pointsToInt points to age" << endl;
+
+	// Displaying the value of pointer
+	cout << "pointsToInt = 0x" << hex << pointsToInt << endl;
+
+	// Displaying the value at the pointed location
+	cout << "*pointsToInt = " << dec << *pointsToInt << endl;
+
+	pointsToInt = &dogsAge;
+	cout << "pointsToInt points to dogsAge now" << endl;
+
+	cout << "pointsToInt = 0x" << hex << pointsToInt << endl;
+	cout << "*pointsToInt = " << dec << *pointsToInt << endl;
+
+	pointsToInt = &testme;
+	cout << "pointsToInt = 0x" << hex << pointsToInt << endl;
+	cout << "*pointsToInt = " << dec << *pointsToInt << endl;
+
+	return 0;
+}
